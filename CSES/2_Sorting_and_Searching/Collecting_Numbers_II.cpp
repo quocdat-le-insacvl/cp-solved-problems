afdcdcd849e621828ab7d512c3f6fa38d5900ac7
LE Quoc Dat <quocdat-le-insacvl@gmail.com>
2024-08-13 18:19:00 +0200

    update template

--- CSES/2_Sorting_and_Searching/Collecting_Numbers_II.cpp
+++ CSES/2_Sorting_and_Searching/Collecting_Numbers_II.cpp
@@ -1,3 +1,4 @@
+
 #pragma GCC optimize ("O3")
 #pragma GCC target ("sse4")
 
@@ -23,6 +24,7 @@ typedef vector<cd> vcd;
 template<class T> using pq = priority_queue<T>;
 template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 
+#define fori(n) for (int i=0; i<n; i++)
 #define FOR(i, a, b) for (int i=a; i<(b); i++)
 #define F0R(i, a) for (int i=0; i<(a); i++)
 #define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
@@ -58,7 +60,7 @@ template<typename T, typename V>
 void __print(const pair<T, V> &x) {cerr << '('; __print(x.f); cerr << "|"; __print(x.s); cerr << ')';}
 template<typename T>
 void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
+void _print() {cerr << "]";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
 #ifdef DEBUG
@@ -77,9 +79,80 @@ const int MOD = 1000000007;
 const char nl = '\n';
 const int MX = 100001; 
 
-void solve() { 
-  
+void solve() {
+  int n, q; cin >> n >> q;
+  vpi a(n);
+  vi ori(n);
+  for (int i=0; i<n; i++) {
+    cin >> a[i].f;
+    a[i].s = i + 1;
+    ori[i] = a[i].f;
+  }
+  // trav(x, a) cin >> x;
+  // vi a(n);
+  // dbg("no sort", a);
+  sort(all(a));
+  dbg(a);
+  vi cnt(n);
+  int ans = 1;
+  for (int i=0; i<sz(a)-1; i++) {
+    if (a[i].s > a[i+1].s){
+      // f = a[i].s;
+      cnt[i] = 1;
+      ans++;
+    }
+  }
+  while (q--) {
+    int x,y; cin >> x >> y;
+    dbg(ori);
+    dbg(x,y);
+    x--; y--;
+    dbg(ori[x], ori[y]);
+    swap(a[ori[x]-1].s, a[ori[y]-1].s);
+    swap(ori[x], ori[y]);
+    dbg(ori);
+    dbg(a);
+    // 3 2 1 4 6
+    // 1 1 0 0 0
+    // swap : [2], [3] (idx here: ori[x]-1, resp y)=> check [2-1]=[1], [2], [3-1]=2,[3]
+    // 3 1 2 4 6
+    //
+    // int cnt = 1;
+    // int f = a[0].s;
+    int ix = ori[x]-1, iy = ori[y]-1;
+    dbg(ix, iy);
+    ans -= cnt[ix] + cnt[iy];
+    if (ix < n - 1) cnt[ix] = a[ix].s > a[ix+1].s;
+    if (iy < n - 1) cnt[iy] = a[iy].s > a[iy+1].s;
+    ans += cnt[ix] + cnt[iy];
+    dbg("ans1", ans);
+    dbg(cnt);
+    if (ix > 0) {
+      ans -= cnt[ix-1];
+      cnt[ix-1] = a[ix-1].s > a[ix].s;
+      ans += cnt[ix-1];
+    }
+    if (iy > 0) {
+      ans -= cnt[iy-1];
+      cnt[iy-1] = a[iy-1].s > a[iy].s;
+      ans += cnt[iy-1];
+    }
+    dbg("ans1", ans);
+    dbg(cnt);
+    cout << ans << '\n';
+
+  }
 }
+
+// void solve() {
+  // int n; cin >> n;
+  // vi a(n); trav(x, a) cin >> x;
+  // int cnt = 1;
+  // for (int i=0; i<n-1; i++) {
+    // if (a[i] > a[i+1]) cnt++;
+  // }
+  // cout << cnt << '\n';
+// }
 
 int main() {
   cin.tie(0)->sync_with_stdio(0);
